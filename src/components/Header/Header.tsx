722bfac7acd59b91e2d0c819b7b02278b380ebae
Danyaell <danyaellmtzo17@gmail.com>
2025-05-18 18:27:31 -0600

    update-header-styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import styles from "./Header.module.css";
 
@@ -8,6 +9,16 @@ export default function Header() {
       <div>
         <h1 className={styles.title}>Sistema del CAE</h1>
         <p className={styles.subtitle}>CENTRO DE APOYO EXTRACURRICULAR</p>
+      </div>
+      <div>
+        <Link to="/reports">
+          <button className={styles.linkButton}>REPORTES</button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/requests">
+          <button className={styles.linkButton}>SOLICITUDES</button>
+        </Link>
       </div>
     </header>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,12 @@
-import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>INICIO</h1>
+      <h1 className={styles.title}>INICIO</h1>
       <br />
       <br />
-      <div>
-        <Link to="/reports">
-          <button className={styles.linkButton}>REPORTES</button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/requests">
-          <button className={styles.linkButton}>SOLICITUDES</button>
-        </Link>
-      </div>
+      
     </div>
   );
 }
