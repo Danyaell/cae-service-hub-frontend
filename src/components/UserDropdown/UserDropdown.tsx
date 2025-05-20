2116982c31f9f2015d8bef5e2a26eade1c3d1207
Danyaell <danyaellmtzo17@gmail.com>
2025-05-20 12:19:19 -0600

    add-dropdown

---
+++ src/components/UserDropdown/UserDropdown.tsx
@@ -0,0 +1,53 @@
+import { useState, useRef, useEffect } from "react";
+import styles from "./UserDropdown.module.css";
+import { FaUserCircle } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { useAuthStrore } from "../../store/login.store";
+
+export const UserDropdown = () => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const user = useAuthStrore((state) => state.user);
+  const logout = useAuthStrore((state) => state.logout);
+
+  // Cierra el dropdown si se hace clic fuera
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <div ref={ref}>
+      <button onClick={() => setOpen(!open)} className={styles.navUserButton}>
+        <FaUserCircle className={open ? styles.navUserIconActive : styles.navUserIcon} />
+      </button>
+
+      {open && (
+        <div className={styles.dropdownContainer}>
+          <div className={styles.dropdownHeader}>¡Hola, {user?.name}!</div>
+          <ul className={styles.dropdownMenu}>
+            <li className={styles.dropdownMenuItem}>
+              <NavLink to="/" className={styles.dropdownLink}>
+                Perfil
+              </NavLink>
+            </li>
+            <li className={styles.dropdownMenuItem}>
+              <NavLink to="/" className={styles.dropdownLink}>
+                Configuración
+              </NavLink>
+            </li>
+          </ul>
+          <button className={styles.logoutButton} onClick={() => logout()}>
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
